4d2cc4b74a37309edfb2e086f4c6a863b3b8460a
Tim Tyree <tjt57@cornell.edu>
2020-08-15 11:34:02 -0700

    consolidated birth-death rates for ic-suite-2

--- notebooks/lib/birth_death_rates_from_ic.py
+++ notebooks/lib/birth_death_rates_from_ic.py
@@ -638,13 +638,13 @@ def _get_kwargs(ic):
 	sigma       = 1.5 #pixels
 	threshold   = 0.6 #unitless 0 to 1
 	V_threshold = 0.5  #unitless 0 to 1
-	edge_tolerance = 3
-	pad = 5
-	atol = 1e-11
+	edge_tolerance = 10#3
+	pad = 20#5
+	atol = 1e-9#1e-11
 	color_values = None
 	h = 0.1 #0.1 for when D=0.0005cm^2/ms, ##0.007) for when D=0.001cm^2/ms, #milliseconds
 	nsteps = 1*10**6
-	save_every_n_frames = 100
+	save_every_n_frames = 10#100
 	max_time = h*nsteps  #milliseconds
 	max_buffers_to_save = 0
 	buffers_saved = 0
@@ -654,7 +654,7 @@ def _get_kwargs(ic):
 	recording = True
 	descrip = f'sigma_{sigma}_threshold_{threshold}'
 	save = True
-	mem = 2 #frames
+	mem  = 2#2 #frames
 	sr   = 50  #pixels
 	ds=5 #cm #width of square domain
 	tmin = 100#milliseconds
