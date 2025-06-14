d2ad366ed74800e8f7becca428ab6c0003a3481f
timtyree <tttyree@ucsd.edu>
2025-06-14 13:57:28 -0700

    dev analysis of runtimes comparing full model to particle model

--- notebooks/lib/measure/get_tips_local.py
+++ notebooks/lib/measure/get_tips_local.py
@@ -1,25 +1,35 @@
 from ..my_initialization import *
+from ._find_tips_kernel_cy import find_intersections
+from .interpolate import interpolate_img
 
 # @njit
-def compute_all_spiral_tips(img,dimgdt,level1,level2,width=200,height=200):
+def compute_all_spiral_tips(img,dimgdt,level1,level2,width=200,height=200,**kwargs):
     #compute all spiral tips present
-    retval = find_intersections(img,dimgdt,level1,level2,theta_threshold=theta_threshold)
+    retval = find_intersections(img,dimgdt,level1,level2,**kwargs)#,theta_threshold=theta_threshold,**kwargs)
+    # retval = find_intersections(img,dimgdt,level1,level2,theta_threshold=theta_threshold)
     # level2=V_threshold
     # retval = find_intersections(img1,img2,level1,level2,theta_threshold=theta_threshold)
     lst_values_x,lst_values_y,lst_values_theta, lst_values_grad_ux, lst_values_grad_uy, lst_values_grad_vx, lst_values_grad_vy = retval
     return format_spiral_tips(lst_values_x,lst_values_y,lst_values_theta, lst_values_grad_ux,
-                              lst_values_grad_uy, lst_values_grad_vx, lst_values_grad_vy)
+                              lst_values_grad_uy, lst_values_grad_vx, lst_values_grad_vy,
+                              img,dimgdt,
+                              width=width,height=height,**kwargs)
 # @njit
-def format_spiral_tips(lst_values_x,lst_values_y,lst_values_theta, lst_values_grad_ux, lst_values_grad_uy, lst_values_grad_vx, lst_values_grad_vy):
+def format_spiral_tips(lst_values_x,lst_values_y,
+            lst_values_theta, lst_values_grad_ux,
+            lst_values_grad_uy, lst_values_grad_vx,
+            lst_values_grad_vy,
+            img,dimgdt,
+            width,height,**kwargs):
     x_values = np.array(lst_values_x)
     y_values = np.array(lst_values_y)
     # EP states given by bilinear interpolation with periodic boundary conditions
     v_lst    = interpolate_img(x_values,y_values,width,height,img=img)
     dvdt_lst = interpolate_img(x_values,y_values,width,height,img=dimgdt)
- 
+
     n_tips = x_values.size
     dict_out = {
-        't': float(t),
+        # 't': float(t),
         'n': int(n_tips),
         'x': list(lst_values_x),
         'y': list(lst_values_y),
